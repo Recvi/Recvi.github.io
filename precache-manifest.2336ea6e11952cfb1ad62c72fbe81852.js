5b3ca8b2bc98477f061072ef63ebafc48f88ec90
Recvi <th.charalampos@gmail.com>
2020-05-31 18:31:43 +0300

    Updates

---
+++ precache-manifest.2336ea6e11952cfb1ad62c72fbe81852.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f53ad3c21110553095dd71e6475ce262",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2da0a71fbda7140693c",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "17c79084c1e20441ba34",
+    "url": "/static/js/2.a489c2db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a489c2db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2da0a71fbda7140693c",
+    "url": "/static/js/main.58097008.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
