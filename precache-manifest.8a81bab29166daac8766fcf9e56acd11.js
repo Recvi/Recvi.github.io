cee2c0754dc711125cbe62c3185d166bc96e3003
Recvi <th.charalampos@gmail.com>
2020-05-31 19:15:13 +0300

    Updates

---
+++ precache-manifest.8a81bab29166daac8766fcf9e56acd11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50f1431122720f6b21dc23ed617005af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e09c05dd0d00768dfc64",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e09c05dd0d00768dfc64",
+    "url": "/static/js/main.79916cde.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
