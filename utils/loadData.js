8d680ad8f41159127174963e7526edcc1580da5f
Recvi <th.charalampos@gmail.com>
2020-07-29 14:03:09 +0000

    Refactored loadData to be more readable.

--- utils/loadData.js
+++ utils/loadData.js
@@ -5,11 +5,27 @@ import fs from 'fs'
 
 const dataDirectory = path.join(process.cwd(), 'data');
 
+/**
+ * Returns json data loaded from every file in `dataDirectory` (files are assumed to `.json`).
+ * Filename without prefix is used to separate data of different files.
+ * ex. {
+ *      personalInfo: {
+ *          name: "Charalampos"
+ *          ...
+ *      }
+ * }
+ * is inside personalInfo.json
+ * 
+ * Data object is passed to props from `getStaticPros` in index page.
+ * 
+ * @returns {Object} data
+ */
 export default function() {
     const data = {};
     fs.readdirSync(dataDirectory).forEach(filename => {
         const fileContents = fs.readFileSync(path.join(dataDirectory, filename), 'utf8')
-        data[filename.split('.')[0]] = JSON.parse(fileContents)
+        const [justName] = filename.split('.')
+        data[justName] = JSON.parse(fileContents)
     })
     return data;
 }
